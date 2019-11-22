56d7ce598d2031d32a7cd2fddf087f131a94cfea
Mayank <mayankchd10@gmail.com>
2019-11-22 23:53:04 +0530

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+#Here we're importing Django's function path 
+# and all of our views from the blog application.
+# (We don't have any yet, but we will get to that in a minute!)
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
+
+#This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
+
+#The last part, name='post_list', is the name of the URL that will be used to identify the view
