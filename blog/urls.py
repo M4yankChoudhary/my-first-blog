440793f1449995c665f292adda388e7ef1d3616c
Mayank <mayankchd10@gmail.com>
2019-11-28 15:20:36 +0530

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 #This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
